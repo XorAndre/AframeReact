e0d5301d0507d15b82f3eedf1fab82919972c264
André Rodrigues (Xor) <xor2017@outlook.com>
2020-07-19 02:17:26 -0300

    Atualizações fim de noite

--- BoilerplateCode/src/App.tsx
+++ BoilerplateCode/src/App.tsx
@@ -3,20 +3,24 @@ import 'aframe';
 import 'aframe-particle-system-component';
 import {Entity, Scene} from 'aframe-react';
 //Componentes
+import { Reset } from 'styled-reset'
 import {
   Theme
 } from './Components/Theme/Styles'
 const App = () => {
   return (
-    <Theme>
-      <Scene>
-        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-        <Entity particle-system={{preset: 'snow'}}/>
-        <Entity light={{type: 'point'}}/>
-        <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
-        <Entity text={{value: 'Hello, WebVR!'}}/>
-      </Scene>
+    <React.Fragment>
+      <Reset />
+      <Theme>
+        <Scene>
+          <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+          <Entity particle-system={{preset: 'snow'}}/>
+          <Entity light={{type: 'point'}}/>
+          <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
+          <Entity text={{value: 'Texto Qualquer!'}}/>
+        </Scene>
     </Theme>
+  </React.Fragment>
   );
 }
 
